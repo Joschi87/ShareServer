e91c8759a77a3ebfa9c8aff72863555283130938
Joschi87 <janosch.walter27@gmail.com>
2021-02-24 20:21:00 +0100

    Comment a method

--- src/main/java/server/service/AuthenticationService.java
+++ src/main/java/server/service/AuthenticationService.java
@@ -1,5 +1,31 @@
 package server.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import server.ifs.UserRepository;
+import server.lib.utils.JwtTokenService;
+
+@Service
 public class AuthenticationService {
 
+	private UserRepository userRepository;
+	
+	private JwtTokenService jwtTokenService;
+	
+	private PasswordEncoder passwordEncoder;
+	
+	public AuthenticationService(UserRepository userRepository, JwtTokenService jwtTokenService, PasswordEncoder passwordEncoder) {
+		this.userRepository = userRepository;
+		this.jwtTokenService = jwtTokenService;
+		this.passwordEncoder = passwordEncoder;
+	}
+	
+	/*public JWTTokenResponse generateJWTToken(String username, String password) {
+        return userRepository.findOneByUsername(username)
+                .filter(account ->  passwordEncoder.matches(password, account.getPassword()))
+                .map(account -> new JWTTokenResponse(jwtTokenService.generateToken(username)))
+                .orElseThrow(() ->  new EntityNotFoundException("Account not found"));
+    }*/
+	
 }
